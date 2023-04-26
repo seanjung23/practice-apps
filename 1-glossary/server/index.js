f9aee75ee00c595ecd2a90d8c43ec97a93d222a5
Sean Jung <sean.jung23@gmail.com>
2023-04-26 17:38:41 -0400

    Created .env file and set up middleware and route in server/index.js successfully

--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -6,14 +6,18 @@ const app = express();
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.json());
 
-/**** 
- * 
- * 
- * Other routes here....
- *
- * 
- */
+const glossary = [
+  { word: 'mendicant', definition: 'a pauper who lives by begging.' },
+  { word: 'meretricious', definition: 'tastelessly showy.' },
+  { word: 'vitiate', definition: 'make imperfect.' },
+  { word: 'vapid', definition: 'lacking significance or liveliness.' },
+];
+
+app.get('/getWords', (req, res) => {
+  res.send(glossary);
+});
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
